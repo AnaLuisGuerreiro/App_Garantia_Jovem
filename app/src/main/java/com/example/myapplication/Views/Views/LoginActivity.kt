f6c610e1b0b775de1861b00eb80ff1a8a813c0ea
anaguerreiro <analuisguerreiro@hotmail.com>
2024-05-30 23:29:12 +0100

    Login view

--- app/src/main/java/com/example/myapplication/Views/Views/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/Views/Views/LoginActivity.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.Views
+package com.example.myapplication.Views.Views
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
